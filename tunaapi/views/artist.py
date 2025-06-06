3df3f028ab093453b3f40a13664dc9a9d5a420bf
Alyssa Cleland <alyssamariecleland@gmail.com>
2025-06-06 02:16:08 -0500

    detailed retrieve single views completed

--- tunaapi/views/artist.py
+++ tunaapi/views/artist.py
@@ -4,6 +4,8 @@ from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from rest_framework import serializers, status
 from tunaapi.models import Artist
+from django.db.models import Count
+from tunaapi.views.song import AllSongsSerializer
 
 
 class ArtistView(ViewSet):
@@ -19,9 +21,31 @@ class ArtistView(ViewSet):
         serializer = AllArtistsSerializer(artists, many=True)
         return Response(serializer.data)
 
+    def retrieve(self, request, pk):
+        try:
+            # Count is a python fx that returns the number of times an object appears in a list.
+            # Annotate is a method that allows you to add a new field to the queryset that is calculated based on the existing fields.
+            # Count('songs') uses the reverse relationship from Artist to Song (thanks to the foreign key artist on Song).
+            artist = Artist.objects.annotate(
+                song_count=Count('songs')).get(pk=pk)
+            serializer = SingleArtistSerializer(artist)
+            return Response(serializer.data)
+        except Artist.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
 
 class AllArtistsSerializer(serializers.ModelSerializer):
     """JSON Serializer for artists"""
     class Meta:
         model = Artist
         fields = ('id', 'name', 'age', 'bio')
+
+
+class SingleArtistSerializer(serializers.ModelSerializer):
+    """JSON Serializer for a single artist including their songs"""
+    songs = AllSongsSerializer(many=True, read_only=True)
+    song_count = serializers.IntegerField(read_only=True)
+
+    class Meta:
+        model = Artist
+        fields = ('id', 'name', 'age', 'bio', 'song_count', 'songs')
